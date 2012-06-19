9352dd4720ace5ff30b7f5f9c38dac882c41f47e
Matt Parrott <wikitopian@gmail.com>
2012-06-19 01:08:28 -0400

    Changed email address on license

--- WPDonationMeta.php
+++ WPDonationMeta.php
@@ -15,7 +15,7 @@
 /**
 License:
 ==============================================================================
-Copyright 2011 Matt Parrott  (email : parrott.matt@gmail.com)
+Copyright 2011 Matt Parrott  (email : matt@swarmstrategies.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
