311ce20596dd0be428e58dacc0fb25a1e24b2042
wikitopian <matt@swarmstrategies.com>
2012-06-20 15:31:03 -0400

    DonatePopUp.php populated

---
+++ DonatePopUp.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Donate now with PayPal...
+		</title>
+	</head>
+	<body>
+		<h2>Please fill in the following required information...</h2>
+		<br />
+		<form id="donate" name="donate">
+			<input type="button" name="donate-submit" value="Continue to PayPal..." />
+		</form>
+	</body>
+</html>
